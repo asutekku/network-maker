4d0b3659a192790505ce11153f9a27a4bb915864
Aku Mäkelä <aku.n.makela@gmail.com>
2020-04-24 13:32:30 +0300

    Remove vis from bundle and add minifier

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -2,16 +2,44 @@
 const merge = require('webpack-merge');
 const {resolve} = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const commonConfig = require('./common');
-
-const srcPath = resolve(__dirname, '../../src');
 const dstPath = resolve(__dirname, '../../dist');
 
 module.exports = merge(commonConfig, {
     mode: 'production',
     entry: './index.tsx',
+    externals: {
+        "react": "React",
+        "vis": "vis"
+    },
     optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                parse: {
+                    ecma: 8
+                },
+                compress: {
+                    ecma: 5,
+                    warnings: false,
+                    comparisons: false,
+                    inline: 2
+                },
+                mangle: {
+                    safari10: true
+                },
+                output: {
+                    ecma: 5,
+                    comments: false,
+                    ascii_only: true
+                }
+            },
+            cache: true,
+            parallel: true,
+            sourceMap: true, // Must be set to true if using source-maps in production
+        })],
         splitChunks: {
             cacheGroups: {
                 styles: {
